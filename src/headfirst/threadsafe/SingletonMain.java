9d881c35c0d8275c3483b631460f496fa3da5225
201611059 김보규 <sa5469@naver.com>
2017-05-16 14:25:07 +0900

    Delete SingletonMain.java

--- src/headfirst/threadsafe/SingletonMain.java
+++
@@ -1,9 +0,0 @@
-package headfirst.singleton.threadsafe;
-
-public class SingletonMain{
-  public static void main(String[] args){
-    Singleton s1 = Singleton.getInstance();
-    Singleton s2 = Singleton.getInstance();
-    Singleton s3 = Singleton.getInstance();
-  }
-}
