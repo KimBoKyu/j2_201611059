edde9cdce21bd53ef4d2143353b90e3dd7b76610
201611059 김보규 <sa5469@naver.com>
2017-05-16 14:25:32 +0900

    Delete SingletonClient.java

--- src/headfirst/dcl/SingletonClient.java
+++
@@ -1,8 +0,0 @@
-package headfirst.singleton.dcl;
-
-public class SingletonClient{
-  public static void main(String[] args){
-    Singleton s1 = Singleton.getInstance();
-    Singleton s2 = Singleton.getInstance();
-  }
-}
